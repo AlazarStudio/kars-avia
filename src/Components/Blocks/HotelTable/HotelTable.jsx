374a798f8faebb047c3db110b2f7f34cd2336e5c
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-07-18 16:22:52 +0300

    Update

--- src/Components/Blocks/HotelTable/HotelTable.jsx
+++ src/Components/Blocks/HotelTable/HotelTable.jsx
@@ -2,29 +2,15 @@ import React, { useState, useEffect } from 'react';
 import Booking from '../Booking/Booking';
 import classes from './HotelTable.module.css';
 
-const HotelTable = () => {
+const HotelTable = ({allRooms, data}) => {
     const [bookings, setBookings] = useState([]);
     const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
     const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
     const today = new Date();
 
     useEffect(() => {
-        const data = [
-            { room: '№121', place: 1, start: '2024-07-01', end: '2024-07-09', client: 'Джатдоев А. С-А.' },
-            { room: '№122', place: 1, start: '2024-07-03', end: '2024-07-10', client: 'Гочияев Р. Р.' },
-            { room: '№221', place: 1, start: '2024-07-12', end: '2024-07-29', client: 'Уртенов А. З.' },
-            { room: '№221', place: 2, start: '2024-07-10', end: '2024-07-19', client: 'Джатдоев А. С-А.' },
-            { room: '№222', place: 2, start: '2024-07-12', end: '2024-07-23', client: 'Гочияев Р. Р.' },
-        ];
         setBookings(data);
     }, []);
-
-    const allRooms = [
-        { room: '№121', places: 1 },
-        { room: '№122', places: 1 },
-        { room: '№221', places: 2 },
-        { room: '№222', places: 2 },
-    ];
 
     const getDaysInMonth = (month, year) => {
         return new Date(year, month + 1, 0).getDate();
--- src/Components/Pages/Placement/Placement.jsx
+++ src/Components/Pages/Placement/Placement.jsx
@@ -7,6 +7,22 @@ import HotelTable from "../../Blocks/HotelTable/HotelTable";
 
 function Placement({ children, ...props }) {
     let { idHotel } = useParams();
+
+    const allRooms = [
+        { room: '№121', places: 1 },
+        { room: '№122', places: 1 },
+        { room: '№221', places: 2 },
+        { room: '№222', places: 2 },
+    ];
+
+    const data = [
+        { room: '№121', place: 1, start: '2024-07-01', end: '2024-07-09', client: 'Джатдоев А. С-А.' },
+        { room: '№122', place: 1, start: '2024-07-03', end: '2024-07-10', client: 'Гочияев Р. Р.' },
+        { room: '№221', place: 1, start: '2024-07-12', end: '2024-07-29', client: 'Уртенов А. З.' },
+        { room: '№221', place: 2, start: '2024-07-10', end: '2024-07-19', client: 'Джатдоев А. С-А.' },
+        { room: '№222', place: 2, start: '2024-07-12', end: '2024-07-23', client: 'Гочияев Р. Р.' },
+    ];
+    
     return (
         <div className={classes.main}>
             <MenuDispetcher />
@@ -21,9 +37,9 @@ function Placement({ children, ...props }) {
                     </Header>
                 </div>
 
-                <HotelTable/>
+                <HotelTable allRooms={allRooms} data={data} />
             </div>
-            
+
         </div>
     );
 }
