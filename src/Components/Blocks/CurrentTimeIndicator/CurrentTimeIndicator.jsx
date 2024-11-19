a57e9b6dda0c2cbe29dd73f87bb5cb165e9ce181
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-11-19 20:09:11 +0300

    Update

--- src/Components/Blocks/CurrentTimeIndicator/CurrentTimeIndicator.jsx
+++ src/Components/Blocks/CurrentTimeIndicator/CurrentTimeIndicator.jsx
@@ -2,13 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Box } from "@mui/material";
 import { differenceInDays, startOfYear } from "date-fns";
 
-function CurrentTimeIndicator({ year = new Date().getFullYear() }) {
+function CurrentTimeIndicator({ year = new Date().getFullYear(), dayWidth }) {
     const [currentDayPosition, setCurrentDayPosition] = useState(0);
 
     useEffect(() => {
         const today = new Date();
         const startOfYearDate = startOfYear(new Date(year, 0, 1));
-        const dayWidth = 30; // Ширина одной ячейки дня
         const daysSinceStartOfYear = differenceInDays(today, startOfYearDate);
         const position = daysSinceStartOfYear * dayWidth;
         setCurrentDayPosition(position);
@@ -18,10 +17,10 @@ function CurrentTimeIndicator({ year = new Date().getFullYear() }) {
         <Box
             sx={{
                 position: "absolute",
-                top: '30px', // Опускаем линию ниже, чтобы не пересекать строку месяца
+                top: '30px',
                 left: `${currentDayPosition}px`,
                 width: "2px",
-                height: "25px", // Высота только строки дней
+                height: "25px",
                 backgroundColor: "red",
                 zIndex: 1,
             }}
--- src/Components/Blocks/Timeline/Timeline.jsx
+++ src/Components/Blocks/Timeline/Timeline.jsx
@@ -1,56 +1,78 @@
-import React from "react";
+import React, { memo } from "react";
 import { Box, Typography } from "@mui/material";
-import { format, eachDayOfInterval, addMonths, startOfMonth, endOfMonth, startOfYear, endOfYear } from "date-fns";
+import { format, eachDayOfInterval, addMonths, startOfMonth, endOfMonth, startOfYear, endOfYear, isToday } from "date-fns";
+import { ru } from "date-fns/locale";
 
-function Timeline({ year = new Date().getFullYear(), dayWidth }) {
+const Timeline = memo(({ year = new Date().getFullYear(), dayWidth, weekendColor, monthColor }) => {
     const months = Array.from({ length: 12 }, (_, i) => startOfMonth(addMonths(new Date(year, 0, 1), i)));
+    const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+    const currentMonthIndex = new Date().getMonth();
 
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', borderBottom: '1px solid #ddd' }}>
             {/* Строка месяцев */}
-            <Box sx={{ display: 'flex', height: '30px', borderBottom: '1px solid #ddd' }}> {/* Принудительная высота строки месяца */}
+            <Box sx={{ display: 'flex', height: '30px', borderBottom: '1px solid #ddd' }}>
                 {months.map((month, index) => {
                     const daysInMonth = eachDayOfInterval({ start: startOfMonth(month), end: endOfMonth(month) });
+                    const isActiveMonth = index === currentMonthIndex;
+
                     return (
                         <Box
                             key={index}
                             sx={{
                                 textAlign: 'center',
-                                borderRight: '1px solid #ddd',
-                                width: `${dayWidth * daysInMonth.length}px`,
-                                backgroundColor: '#f5f5f5',
-                                padding: '4px 0'
+                                borderRight: `3px solid ${monthColor}`,
+                                width: `${Math.round(dayWidth * daysInMonth.length)}px`,
+                                backgroundColor: isActiveMonth ? '#f3f292' : '#f5f5f5',
+                                padding: '4px 0',
                             }}
                         >
                             <Typography variant="caption" sx={{ fontWeight: 'bold' }}>
-                                {format(month, 'MMMM yyyy')}
+                                {capitalizeFirstLetter(format(month, 'LLLL yyyy', { locale: ru }))}
                             </Typography>
                         </Box>
                     );
                 })}
             </Box>
 
+
             {/* Строка дней */}
-            <Box sx={{ display: 'flex', height: '30px' }}> {/* Принудительная высота строки дней */}
-                {eachDayOfInterval({ start: startOfYear(new Date(year, 0, 1)), end: endOfYear(new Date(year, 11, 31)) }).map((day, index) => (
-                    <Box
-                        key={index}
-                        sx={{
-                            width: `${dayWidth}px`,
-                            textAlign: 'center',
-                            borderRight: index % 7 === 6 ? '2px solid #ddd' : '1px solid #ddd',
-                            backgroundColor: '#fff',
-                            padding: '2px 0'
-                        }}
-                    >
-                        <Typography variant="caption">
-                            {format(day, 'd')}
-                        </Typography>
-                    </Box>
-                ))}
+            <Box sx={{ display: 'flex', height: '30px' }}>
+                {eachDayOfInterval({ start: startOfYear(new Date(year, 0, 1)), end: endOfYear(new Date(year, 11, 31)) }).map((day, index) => {
+                    const isWeekend = day.getDay() === 0 || day.getDay() === 6;
+                    const isLastDayOfMonth = day.getDate() === endOfMonth(day).getDate();
+                    const isCurrentDay = isToday(day);
+
+                    return (
+                        <Box
+                            key={index}
+                            sx={{
+                                width: `${dayWidth}px`,
+                                textAlign: 'center',
+                                borderRight: isLastDayOfMonth ? `3px solid ${monthColor}` : '1px solid #ddd',
+                                padding: '2px 0',
+                                backgroundColor: isCurrentDay
+                                    ? '#f3f292'
+                                    : isWeekend
+                                        ? weekendColor
+                                        : '#fff',
+                            }}
+                        >
+                            <Typography
+                                variant="caption"
+                                sx={{
+                                    fontWeight: isCurrentDay ? 'bold' : 'normal',
+                                }}
+                            >
+                                {format(day, 'd', { locale: ru })}
+                            </Typography>
+                        </Box>
+                    );
+                })}
             </Box>
+
         </Box>
     );
-}
+})
 
 export default Timeline;
